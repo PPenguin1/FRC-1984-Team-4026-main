0f899598deaffe6d956e9f274abb0d781556f560
Charles Kennedy <82charkenn@csdecatur.net>
2024-10-24 17:50:32 -0400

    added things to arm subsystem

---
+++ RobotCode/src/main/java/frc/robot/constants/ArmConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.constants;
+
+public class ArmConstants {
+
+    public static final double LOWER_ARNGLE_MIN_POSTION = 0;
+    public static final double UPPER_ARNGLE_MIN_POSTION = 0;
+    
+}
--- RobotCode/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ RobotCode/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,8 +1,50 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.ArmConstants;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 public class ArmSubsystem extends SubsystemBase
 {
+    private TalonFX armKrakenMotorLower, armKrakenMotorUpper;
+
+    private double targetPosition;
+
+    private double minArmAngle;
+
+    public ArmSubsystem(int armKrakenMotorLowerPort, int armKrakenMotorUpperPort)
+    {
+        armKrakenMotorLower = new TalonFX(armKrakenMotorLowerPort);
+        armKrakenMotorUpper = new TalonFX(armKrakenMotorUpperPort);
+
+        TalonFXConfiguration mainMotorconfigs = new TalonFXConfiguration();
+
+        armKrakenMotorLower.getConfigurator().apply(mainMotorconfigs);
+        armKrakenMotorUpper.getConfigurator().apply(mainMotorconfigs);
+
+        minArmAngle = 1/360.0;
+        targetPosition = 1/360.0;
+    }
+
+    public void setArmPositionLower(double position)
+    {
+        targetPosition = position;
+        System.out.println(position);
+        
+        this.targetPosition = Math.max(Math.min(targetPosition, ArmConstants.LOWER_ARNGLE_MIN_POSTION), position );
+
+    }
+
+    public void setArmPositionUpper(double position)
+    {
+        targetPosition = position;
+        System.out.println(position);
+        
+        this.targetPosition = Math.max(Math.min(targetPosition, ArmConstants.UPPER_ARNGLE_MIN_POSTION), position );
+
+    }
+
     
 }
